ce1edec7eb93c34ff7fc9b81c3e0a4f5aefe1646
Saqib Bamualim <saqib.abud@gmail.com>
2018-04-29 20:51:14 +0700

    update database

--- api.py
+++ api.py
@@ -1,13 +1,68 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
-from model import *
 
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+
+news_status = {'deleted', 'draft', 'publish'}
+
+def news_status_validation(status):
+    if status in news_status:
+        return True
+    return False
+
+news_topics = db.Table('news_topics',
+    db.Column('newsid', db.Integer, db.ForeignKey('news.newsid'), primary_key=True),
+    db.Column('topicid', db.Integer, db.ForeignKey('topics.topicid'), primary_key=True)
+)
+
+class News(db.Model):
+    newsid = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+    topics = db.relationship('Topics', secondary=news_topics, lazy="subquery",
+        backref=db.backref('news', lazy=True))
+
+    def as_json(self, topics=True):
+        result = dict(
+            newsid  = self.newsid,
+            title   = self.title,
+            content = self.content,
+            status  = self.status
+            )
+        if topics == True:
+            result['topics'] = []
+            for topic in self.topics:
+                result['topics'].append(topic.as_json(False))
+        return result
+
+    def changeStatus(self, status):
+        if news_status_validation(status):
+            self.status = status
+        else:
+            abort(404, message="Status '{}' doesn't exist".format(status))
+
+class Topics(db.Model):
+    topicid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def as_json(self, news=True):
+        result = dict(
+            topicid  = self.topicid,
+            name     = self.name
+            )
+        if news == True:
+            result['news'] = []
+            for news in self.news:
+                result['news'].append(news.as_json(False))
+        return result
+
+#####################################################################
 
 class NewsResource(Resource):
     def get(self, news_id):
@@ -18,7 +73,7 @@ class NewsResource(Resource):
         news = News.query.get_or_404(news_id)
         news.changeStatus('deleted')
         db.session.commit()
-        return '', 204
+        return news, 204
 
     def put(self, news_id):
         news = News.query.get_or_404(news_id)
@@ -66,7 +121,7 @@ class NewsList(Resource):
         for news in news_list:
             result.append(news.as_json())
         return jsonify(dict(
-            news= result
+            news_list= result
             )
         )
 
@@ -88,7 +143,7 @@ class NewsList(Resource):
             new_news.topics.append(topic)        
         db.session.add(new_news)
         db.session.commit()
-        return "", 201
+        return "success", 201
 
 class TopicResource(Resource):
     def get(self, topic_id):
@@ -98,7 +153,7 @@ class TopicResource(Resource):
     def delete(self, topic_id):
         db.session.delete(Topics.query.get(topic_id))
         db.session.commit()
-        return '', 204
+        return 'success', 204
 
     def put(self, topic_id):
         topic = Topics.query.get_or_404(topic_id)
@@ -113,7 +168,7 @@ class TopicList(Resource):
         for topic in topics:
             result.append(topic.as_json())
         return jsonify(dict(
-                topics= result
+                topics_list= result
             ))
 
     def post(self):
@@ -121,7 +176,7 @@ class TopicList(Resource):
         new_topic.name = request.form['name']
         db.session.add(new_topic)
         db.session.commit()
-        return "", 201
+        return 'success', 201
 
 #################################################################################
 
